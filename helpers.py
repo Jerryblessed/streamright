b11a575057e97dec532ecf094893e0972c90abe5
Jeremiah Ope <106873857+Jerryblessed@users.noreply.github.com>
2023-11-15 15:32:08 +0100

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -9,7 +9,7 @@ from os.path import exists
 from datetime import datetime
 from email.message import EmailMessage
 from passlib.hash import sha256_crypt
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, Flask, flash, request, session, redirect
 from forms import (
     loginForm,
     signUpForm,
@@ -21,21 +21,9 @@ from forms import (
     changeUserNameForm,
     changeProfilePictureForm,
 )
-from flask import (
-    Flask,
-    flash,
-    request,
-    session,
-    redirect,
-    Blueprint,
-    render_template,
-    send_from_directory,
-)
-
 
 def currentDate():
     return datetime.now().strftime("%d.%m.%y")
-
 
 def currentTime(seconds=False):
     if not seconds:
@@ -43,27 +31,29 @@ def currentTime(seconds=False):
     else:
         return datetime.now().strftime("%H:%M:%S")
 
-
 def message(color, message):
-    with open("log.log", "a") as logFile:
-        logFile.write(f"[{currentDate()}|{currentTime(True)}] {message}\n")
     print(
         f"\n\033[94m[{currentDate()}\033[0m"
         f"\033[95m {currentTime(True)}]\033[0m"
         f"\033[9{color}m {message}\033[0m\n"
     )
-
+    logFile = open("log.log", "a")
+    logFile.write(f"[{currentDate()}" f"|{currentTime(True)}]" f" {message}\n")
+    logFile.close()
 
 def addPoints(points, user):
     connection = sqlite3.connect("db/users.db")
     cursor = connection.cursor()
-    cursor.execute('UPDATE users SET points = points + ? WHERE userName = ?', (points, user))
+    cursor.execute(
+        f'update users set points = points+{points} where userName = "{user}"'
+    )
     connection.commit()
     message("2", f'{points} POINTS ADDED TO "{user}"')
-
 
 def getProfilePicture(userName):
     connection = sqlite3.connect("db/users.db")
     cursor = connection.cursor()
-    cursor.execute('SELECT profilePicture FROM users WHERE lower(userName) = ?', (userName.lower(),))
+    cursor.execute(
+        f'select profilePicture from users where lower(userName) = "{userName.lower()}"'
+    )
     return cursor.fetchone()[0]
